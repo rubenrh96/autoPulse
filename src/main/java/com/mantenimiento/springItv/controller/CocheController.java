649d145bf90ab80df12687cf0d5089b83eef8632
rubenrh96 <rubenrh96.rr@gmail.com>
2025-06-27 10:35:32 +0200

    Cambios PROD
    Cambios para que funcione bien pro

--- src/main/java/com/mantenimiento/springItv/controller/CocheController.java
+++ src/main/java/com/mantenimiento/springItv/controller/CocheController.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
 import java.text.SimpleDateFormat;
@@ -49,75 +50,25 @@ public class CocheController {
     }
 
 
-    @PostMapping("/{matricula}")
-    @ResponseBody
-    public String updateKilometraje(@PathVariable String matricula, @RequestParam("kilometros") int kilometros) {
-        cocheService.obtenerPorId(matricula).ifPresent(coche -> {
-            coche.setKilometros(kilometros);
-            cocheService.guardarCoche(coche);
+    @PostMapping("/{matricula}/kilometros")
+    public String actualizarKilometraje(@PathVariable String matricula,
+                                        @RequestParam int kilometros,
+                                        RedirectAttributes attrs) {
+
+        cocheService.obtenerPorId(matricula).ifPresent(c -> {
+            c.setKilometros(kilometros);
+            cocheService.guardarCoche(c);
         });
-        return "redirect:/coches/" + matricula;
+
+        attrs.addFlashAttribute("success", "Kilometraje actualizado");
+        return "redirect:/coches/" + matricula + "?success=kilometraje" ;
     }
+
 
     @GetMapping("/{matricula}")
     public String verDetalles(@PathVariable String matricula, Model model) {
         cocheService.obtenerPorId(matricula).ifPresent(c -> model.addAttribute("coche", c));
         return "coche/detallesCoche";
-    }
-
-//    @PostMapping
-//    public ResponseEntity<CocheEntity> crearCoche(@RequestBody CocheEntity coche) {
-//        CocheEntity nuevoCoche = cocheService.guardarCoche(coche);
-//        return ResponseEntity.status(201).body(nuevoCoche);
-//    }
-
-    @GetMapping("/{matricula}/modal")
-    public ResponseEntity<Map<String, String>> mostrarMensajeModal(@PathVariable String matricula) {
-        List<MantenimientoEntity> mantenimientos = mantenimientoService.listarMantenimientos(matricula);
-        List<MantenimientoEntity> listaMotor = new ArrayList<>();
-        List<MantenimientoEntity> listaCambio = new ArrayList<>();
-        String mensajeMotor = "", mensajeCambio = "";
-
-        for (MantenimientoEntity mantenimiento : mantenimientos) {
-            if (Long.valueOf(2).equals(mantenimiento.getCategoria().getIdCategoria())) {
-                listaMotor.add(mantenimiento);
-            } else if (Long.valueOf(3).equals(mantenimiento.getCategoria().getIdCategoria())) {
-                listaCambio.add(mantenimiento);
-            }
-        }
-
-        listaMotor.sort(Comparator.comparing(MantenimientoEntity::getFecha).reversed());
-        listaCambio.sort(Comparator.comparing(MantenimientoEntity::getFecha).reversed());
-
-        if (!listaMotor.isEmpty()) {
-            int km = listaMotor.get(0).getKmMantenimiento();
-            int diferencia = (km + 20000) - listaMotor.get(0).getCoche().getKilometros();
-            mensajeMotor = "Quedan <strong>" + diferencia + " km</strong> para cambiar el <strong>aceite del motor.</strong>";
-        }
-
-        if (!listaCambio.isEmpty()) {
-            int km = listaCambio.get(0).getKmMantenimiento();
-            int diferencia = (km + 60000) - listaCambio.get(0).getCoche().getKilometros();
-            mensajeCambio = "Quedan <strong>" + diferencia + " km</strong> para cambiar el <strong>aceite del cambio.</strong>";
-        }
-
-        List<ItvEntity> listaItvs = itvService.listarItvs(matricula);
-        List<Itv> listaItvsModel = listaItvs.stream()
-                .map(TransformadorItv::itvEntityToItv)
-                .sorted(Comparator.comparing(Itv::getKmRevision).reversed())
-                .collect(Collectors.toList());
-
-        Map<String, String> response = new HashMap<>();
-        response.put("mensajeModalMotor", mensajeMotor);
-        response.put("mensajeModalCambio", mensajeCambio);
-
-        if (!listaItvsModel.isEmpty()) {
-            Itv itv = listaItvsModel.get(0);
-            String fechaFormateada = new SimpleDateFormat("dd-MM-yyyy").format(itv.getFechaProximaItv());
-            response.put("mensajeModalItv", "La fecha de próxima ITV es: <strong>" + fechaFormateada + "</strong>");
-        }
-
-        return ResponseEntity.ok(response);
     }
 
     @GetMapping("/nuevo")
